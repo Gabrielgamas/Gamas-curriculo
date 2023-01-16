8774de60bd65844fbad7b13f16095f6d8da4d23f
Gabriel Almeida de Moura Alves <gamas.enge@hotmail.com>
2023-01-16 16:56:46 -0300

    Adição de contatos
    Nessa atualização adicionei a arte de contatos, onde eu faço um formulário para envio de e-mails

--- JavaScript/mostrarMenu.js
+++ JavaScript/mostrarMenu.js
@@ -1,7 +1,16 @@
 const menuImg = document.querySelector(".cabecalho__img");
 const menuTxt = document.querySelector(".cabecalho__menu");
-
+const todoOProjeto = document.querySelectorAll("section");
+console.log(todoOProjeto);
 menuImg.addEventListener("click", () => {
   menuTxt.classList.toggle("mostrarMenu");
   console.log(menuTxt);
 });
+
+for (const todos of todoOProjeto) {
+  todos.addEventListener("click", () => {
+    if (menuTxt.classList.contains("mostrarMenu")) {
+      menuTxt.classList.remove("mostrarMenu");
+    }
+  });
+}
